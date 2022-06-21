35c0475908724134f4e50c5e76b6202a38acfeb5
hui mao <409788696@qq.com>
2022-06-21 16:30:39 +0800

    python-docx

---
+++ word/testpywin32.py
@@ -0,0 +1,33 @@
+"""
+@author:maohui
+@time:2022/6/21 9:18
+  　　　　　　　 ┏┓    ┏┓+ +
+  　　　　　　　┏┛┻━━━━┛┻┓ + +
+  　　　　　　　┃        ┃ 　 
+  　　　　　　　┃     ━  ┃ ++ + + +
+  　　　　　 　████━████ ┃+
+  　　　　　　　┃        ┃ +
+  　　　　　　　┃   ┻    ┃
+  　　　　　　　┃        ┃ + +
+  　　　　　　　┗━┓   ┏━━┛
+  　　　　　　　  ┃   ┃
+  　　　　　　　  ┃   ┃ + + + +
+  　　　　　　　  ┃   ┃　　　Code is far away from bug with the animal protecting
+  　　　　　　　  ┃   ┃+ 　　　　神兽保佑,代码无bug
+  　　　　　　　  ┃   ┃
+  　　　　　　　  ┃   ┃　　+
+  　　　　　　　  ┃   ┗━━━━━━━┓ + +     
+  　　　　　　　  ┃           ┣┓
+  　　　　　　　  ┃           ┏┛
+  　　　　　　　  ┗┓┓┏━━━━━┳┓┏┛ + + + +
+  　　　　　　　   ┃┫┫     ┃┫┫
+  　　　　　　　   ┗┻┛     ┗┻┛+ + + +
+"""
+
+from win32com.client import Dispatch
+from win32com.client.gencache import EnsureDispatch
+from win32com.client import constants
+
+wordpro=EnsureDispatch('Word.Application')
+wordpro.Visible=True
+word1=wordpro.Documents.Add()
